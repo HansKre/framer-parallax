8f212bad51f6db5f4b31ab4408841983f865c61f
Hans <hans.krebs@me.com>
2023-03-01 16:43:49 +0100

    Migrate from background-img to img

--- src/App.tsx
+++ src/App.tsx
@@ -24,6 +24,8 @@ type SectionProps = {
   id: string;
 };
 
+/* use this helper-hook instead of:
+const y = useTransform(scrollYProgress, [0, 1], ['-300%', '300%']); */
 function useParallax(value: MotionValue<number>, distance: number | string) {
   const range =
     typeof distance === 'number'
@@ -32,20 +34,33 @@ function useParallax(value: MotionValue<number>, distance: number | string) {
   return useTransform<number[], number | string>(value, [0, 1], range);
 }
 
+function getImgUrl(id: string) {
+  switch (id) {
+    case '1':
+      return 'https://picsum.photos/1600/1000';
+    case '2':
+      return 'https://picsum.photos/1600/1001';
+    case '3':
+    default:
+      return 'https://picsum.photos/1600/1007';
+  }
+}
+
 function Section({ id }: SectionProps) {
   const ref = useRef(null);
   const { scrollYProgress } = useScroll({
     target: ref,
     offset: ['end start', 'start end'],
   });
-  // const y = useTransform(scrollYProgress, [0, 1], ['-300%', '300%']);
   const y = useParallax(scrollYProgress, '300%');
 
   useMotionValueEvent(scrollYProgress, 'change', (v) => console.log(id, v));
 
   return (
     <section>
+      {/* this is the scroll-reference for the scroll-progress. For the scroll-progress-offset to work, the target and container cannot be in a parent-child relationship */}
       <div ref={ref} />
+      <img src={getImgUrl(id)} alt={id} />
       <motion.h1 style={{ y }}>{`This is section ${id}`}</motion.h1>
     </section>
   );
